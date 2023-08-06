1ff703dd10c08479a52697af8aa3467f47765461
lashagelashvili <las.gelashvili@gmail.com>
2023-08-06 19:27:02 +0400

    finish categories service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from 'src/schemas/categories.schema';
+import { ICategory } from './intefaces/category.interface';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectModel('Category') private categoryModel: Model<Category>,
+  ) {}
+
+  async getCategories() {
+    const categories = await this.categoryModel.find();
+    return categories;
+  }
+
+  async getCategoryById(id: string) {
+    const category = await this.categoryModel.findById(id);
+
+    if (!category) {
+      throw new HttpException(
+        'The category with the given ID was not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return category;
+  }
+
+  async createCategory(categoryBody: ICategory) {
+    let category = new this.categoryModel({
+      name: categoryBody.name,
+      icon: categoryBody.icon,
+      color: categoryBody.color,
+    });
+
+    category = await category.save();
+
+    if (!category) {
+      throw new HttpException(
+        'the category cannot be created!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return category;
+  }
+
+  async updateCategory(id: string, categoryBody: ICategory) {
+    const category = await this.categoryModel.findByIdAndUpdate(
+      id,
+      categoryBody,
+      { new: true },
+    );
+
+    if (!category) {
+      throw new HttpException(
+        'the category cannot be updated!',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return category;
+  }
+
+  async deleteCategory(id: string) {
+    return this.categoryModel
+      .findByIdAndDelete(id)
+      .then((category) => {
+        if (category) {
+          return { success: true, message: 'the category has been deleted' };
+        } else {
+          throw new HttpException(
+            { success: false, message: 'category not found' },
+            HttpStatus.NOT_FOUND,
+          );
+        }
+      })
+      .catch((err) => {
+        throw new HttpException(
+          { success: false, error: err },
+          HttpStatus.BAD_REQUEST,
+        );
+      });
+  }
+}
