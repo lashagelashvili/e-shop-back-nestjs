bf1b423310c0240960e2b1d0f0b8f913645802ed
lashagelashvili <las.gelashvili@gmail.com>
2023-08-06 19:28:06 +0400

    add mongoose to categories module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategorySchema } from 'src/schemas/categories.schema';
 
 @Module({
   providers: [CategoriesService],
   controllers: [CategoriesController],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+  ],
 })
 export class CategoriesModule {}
