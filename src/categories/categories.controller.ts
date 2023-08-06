17457b34096c8be315e1734747d1e65b89b90b01
lashagelashvili <las.gelashvili@gmail.com>
2023-08-06 20:07:38 +0400

    add categories controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoryDto } from './dto/category.dto';
+import { ICategory } from './intefaces/category.interface';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+
+  @Get()
+  getCategories() {
+    return this.categoriesService.getCategories();
+  }
+
+  @Get(':id')
+  categoryById(@Param('id') id: string): Promise<ICategory> {
+    return this.categoriesService.getCategoryById(id);
+  }
+
+  @Post()
+  createCategory(@Body() category: CategoryDto) {
+    return this.categoriesService.createCategory(category);
+  }
+
+  @Post(':id')
+  updateCategory(@Param('id') id: string, @Body() category: CategoryDto) {
+    return this.categoriesService.updateCategory(id, category);
+  }
+
+  @Delete(':id')
+  deleteCategory(@Param('id') id: string) {
+    return this.categoriesService.deleteCategory(id);
+  }
+}
