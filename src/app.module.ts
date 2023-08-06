797faaabaf4519ccc5f37d5bb78ccc2175f887ee
lashagelashvili <las.gelashvili@gmail.com>
2023-08-06 19:50:39 +0400

    configmodule, connect mongoose, cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CategoriesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CategoriesModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster-test.str4b3j.mongodb.net/eshop-database`,
+    ),
+  ],
 })
 export class AppModule {}
